8651b9c5462d57f05d9dc3d9cfa677e289989d09
UrPo2006 <romaibrahim@gmail.com>
2026-06-14 20:26:40 +0300

    lint test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,7 @@ export default async function Home({ params }: PageProps) {
   const { locale } = await params;
 
   return (
-    <main className="bg-slate-50 min-h-screen flex flex-col items-center justify-center p-6 text-center">
+    <main className="bg-maroon-500 min-h-screen flex flex-col items-center justify-center p-6 text-center">
       <div className="bg-white p-8 rounded-2xl shadow-sm border border-slate-100 max-w-md w-full flex flex-col items-center">
         {/* Language Switcher */}
         <div className="mb-6 w-full flex justify-end">
