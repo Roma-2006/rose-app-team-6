2caf0bdd57d53520099a9750776335574f320a92
RowidaElshenawy <rowidaalielshenawy@gmail.com>
2026-06-16 01:38:56 +0300

    resolve globals.css conflict

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,6 @@
+"use client"
 import LanguageSwitcher from '@/components/LanguageSwitcher';
+import Basebutton from '@/shared/components/custom-ui/Basebutton';
 
 interface PageProps {
   params: Promise<{ locale: string }>;
@@ -6,8 +8,10 @@ interface PageProps {
 
 export default async function Home({ params }: PageProps) {
   const { locale } = await params;
-
+  const handleLog=()=>console.log("Hello Rowidaa")
   return (
+    <>
+    <Basebutton handleClick={handleLog}/>
     <main className="bg-slate-50 min-h-screen flex flex-col items-center justify-center p-6 text-center">
       <div className="bg-white p-8 rounded-2xl shadow-sm border border-slate-100 max-w-md w-full flex flex-col items-center">
         {/* Language Switcher */}
@@ -18,12 +22,13 @@ export default async function Home({ params }: PageProps) {
         <h1 className="text-3xl font-extrabold text-slate-900 mb-2 tracking-tight">
           {locale === 'ar' ? 'مرحباً بك في تطبيق روز' : 'Welcome to Rose App'}
         </h1>
-
+       
         <div className="inline-flex items-center gap-2 px-4 py-2 bg-slate-100 rounded-full text-sm font-medium text-slate-700">
           <span>Active Locale:</span>
           <span className="uppercase text-indigo-600 font-bold">{locale}</span>
         </div>
       </div>
     </main>
+    </>
   );
 }
--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -4,22 +4,24 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/shared/lib/utils/tailwind-cn';
 
 const buttonVariants = cva(
-  "group/button inline-flex shrink-0 items-center justify-center rounded-4xl border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "disabled:bg-zinc-300 disabled:text-zinc-500   dark:disabled:bg-zinc-700 dark:disabled:text-zinc-600  group/button inline-flex shrink-0 items-center justify-center   border border-transparent bg-clip-padding text-base rounded-10 font-medium dark:font-semibold dark:text-sm whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none  aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        primary: 'bg-maroon-600 text-white  hover:bg-maroon-700  dark:bg-soft-pink-300 dark:text-zinc-800 dark:hover:bg-soft-pink-400 ',
         outline:
-          'border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-transparent dark:hover:bg-input/30',
+          'border border-maroon-600 bg-white text-maroon-600 hover:bg-maroon-50 disabled:bg-zinc-100 disabled:border-zinc-300 disabled:text-zinc-400  dark:bg-zinc-800 dark:border-soft-pink-300 dark:text-soft-pink-300 dark:hover:bg-zinc-700 dark:disabled:border-zinc-600 dark:disabled:text-zinc-600 dark:disabled:bg-zinc-800  aria-expanded:bg-muted aria-expanded:text-foreground ',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground',
+          'bg-maroon-50 text-maroon-600 hover:bg-maroon-100  dark:bg-zinc-700 dark:text-soft-pink-300 dark:hover:bg-zinc-600 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground',
+        subtle:'bg-zinc-50 border border-zinc-400 text-zinc-800 hover:bg-zinc-100 disabled:bg-zinc-100 disabled:border-zinc-300 disabled:text-zinc-400  dark:bg-zinc-800 dark:border-zinc-500 dark:text-zinc-50 dark:hover:bg-zinc-700 dark:disabled:bg-zinc-800 dark:disabled:border-zinc-600  ',
         ghost:
-          'hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50',
+          ' text-zinc-800 hover:bg-zinc-100 disabled:text-zinc-400 disabled:bg-zinc-100  dark:text-zinc-50 dark:hover:bg-zinc-700   aria-expanded:bg-muted aria-expanded:text-foreground ',
         destructive:
-          'bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40',
+          'bg-red-600 text-white hover:bg-red-700  dark:bg-red-500 dark:text-zinc-50  dark:hover:bg-red-600',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
+        custom:'h-11 w-45.25 gap-1.5',
         default:
           'h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5',
         xs: "h-6 gap-1 px-2.5 text-xs has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
@@ -32,7 +34,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   }
@@ -40,7 +42,7 @@ const buttonVariants = cva(
 
 function Button({
   className,
-  variant = 'default',
+  variant = 'primary',
   size = 'default',
   ...props
 }: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
