73626fb97e601d670006203d27d598eb6dbe496d
NesreenAbotaleb <mohamedaboelhawey@gmail.com>
2026-06-15 18:28:19 +0300

    - setup(design-system) : add themes with next-theme

--- src/shared/providers/theme.provider.tsx
+++ src/shared/providers/theme.provider.tsx
@@ -6,9 +6,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
+      defaultTheme="light"
+      enableSystem={false}
       storageKey="rose-theme"
     >
       {children}
