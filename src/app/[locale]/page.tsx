707663096b8faa01b53cef6d6bfef094b152bdc2
NesreenAbotaleb <mohamedaboelhawey@gmail.com>
2026-06-16 15:54:54 +0300

    test husky

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ interface PageProps {
   params: Promise<{ locale: string }>;
 }
 
-//husky test
+//husky test-2
 
 export default async function Home({ params }: PageProps) {
   const { locale } = await params;
