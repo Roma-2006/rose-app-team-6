3161559a930b65e75403983e9d4b39eae5b4190f
UrPo2006 <romaibrahim1w@gmail.com>
2026-07-01 03:10:56 +0300

    fix: improve banner image quality and update layout layout semantic background

--- src/features/auth/components/language-switcher-auth.tsx
+++ src/features/auth/components/language-switcher-auth.tsx
@@ -23,7 +23,7 @@ export function LanguageSwitcherAuth() {
       type="button"
       onClick={handleSwitchLanguage}
       aria-label={t('auth.langLabel')}
-      className=" cursor-pointer"
+      className=" cursor-pointer text-text-inverse"
     >
       {t('auth.switchLang')}
     </button>
