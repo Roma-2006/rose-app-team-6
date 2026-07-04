c5dadfe554b26a852577f407626afe8f1207a2d3
Asmaa Abdelfatah <abdoasmaa959@gmail.com>
2026-07-04 07:27:27 +0300

    update folders

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,5 +1,4 @@
-import UseLogin from '@/features/auth/hooks/use-login';
-import LoginForm from './_components/login-form';
+import LoginForm from '@/features/auth/components/login-form';
 import { Link } from '@/i18n/navigation';
 import { getTranslations } from 'next-intl/server';
 
