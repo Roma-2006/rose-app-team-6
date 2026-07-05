20f6896183f67bdb106143c0209161072f053a35
UrPo2006 <romaibrahim1w@gmail.com>
2026-07-05 23:42:52 +0300

    update auth layout

--- src/features/auth/components/language-switcher-auth.tsx
+++ src/features/auth/components/language-switcher-auth.tsx
@@ -23,7 +23,7 @@ export function LanguageSwitcherAuth() {
       type="button"
       onClick={handleSwitchLanguage}
       aria-label={t('auth.langLabel')}
-      className=" cursor-pointer text-text-inverse"
+      className=" cursor-pointer text-text-plaint"
     >
       {t('auth.switchLang')}
     </button>
