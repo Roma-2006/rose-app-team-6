279e222bbd7294c44766182af48e119816047c2d
Menna-sultan <mennasultan491@gmail.com>
2026-07-20 15:10:01 +0300

    fix errors2

--- src/app/[locale]/(dashboard)/page.tsx
+++ src/app/[locale]/(dashboard)/page.tsx
@@ -1,13 +1,8 @@
 import HomeContent from '@/features/dashboard/components/home/home-content';
-import { BestSellingSection } from '@/features/dashboard/components/home/home-products/bestSellingSection';
-import { MostPopularSection } from '@/features/dashboard/components/home/home-products/mostPopularSection';
-
 export default async function HomePage() {
   return (
     <>
       <HomeContent />
-      <BestSellingSection />
-      <MostPopularSection />
     </>
   );
 }
