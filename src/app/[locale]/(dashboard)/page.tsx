616b6035a857e59740cef153083b9da9295cdb6e
Menna-sultan <mennasultan491@gmail.com>
2026-07-20 15:52:10 +0300

    update3

--- src/app/[locale]/(dashboard)/page.tsx
+++ src/app/[locale]/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import HomeContent from '@/features/dashboard/components/home/home-content';
-import { BestSellingSection } from '@/features/dashboard/components/home/home-products/bestSellingSection';
-import { MostPopularSection } from '@/features/dashboard/components/home/home-products/mostPopularSection';
+import { BestSellingSection } from '@/features/dashboard/components/home/home-products/BestSellingSection';
+import { MostPopularSection } from '@/features/dashboard/components/home/home-products/MostPopularSection';
 export default async function HomePage() {
   return (
     <>
