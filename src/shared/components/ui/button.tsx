62e68d60a9eb2ef6bf3db9a6dfba6aaba9233261
NesreenAbotaleb <mohamedaboelhawey@gmail.com>
2026-07-27 08:28:15 +0300

    - feat (user-Menu) : fix the Button component which had error distroing the user-dropdown and Login components

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -108,7 +108,11 @@ function Button(props: ButtonPrimitive.Props & VariantProps<typeof buttonVariant
         <>
           {isText && props.leftIcon}
 
-          {isIcon ? props.iconOnly : isText ? t(props.title || '') : props.number}
+          {isIcon
+            ? props.iconOnly
+            : isText
+              ? (props.children ?? t(props.title || ''))
+              : props.number}
           {isText && props.rightIcon}
         </>
       )}
