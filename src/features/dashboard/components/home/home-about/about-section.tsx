4dab12a04bdd8f2bad416d464eaefce76e985baa
UrPo2006 <romaibrahim1w@gmail.com>
2026-08-01 20:32:36 +0300

    Uploaded the sprint to the testing

--- src/features/dashboard/components/home/home-about/about-section.tsx
+++ src/features/dashboard/components/home/home-about/about-section.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-import SecHeader from '../section-header';
-import { Button } from '@/shared/components/ui/button';
+
 import { Check } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
+import SecHeader from '../section-header';
+import { Button } from '@/shared/components/ui/button';
 
 export default async function AboutSection() {
   const tAbout = await getTranslations('home.about');
