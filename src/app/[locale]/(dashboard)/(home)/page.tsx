f2de005e648e4787b1edba428bca57d1a8ff3473
UrPo2006 <romaibrahim1w@gmail.com>
2026-08-03 17:14:20 +0300

    Save my local changes on home page

--- src/app/[locale]/(dashboard)/(home)/page.tsx
+++ src/app/[locale]/(dashboard)/(home)/page.tsx
@@ -1,8 +1,9 @@
 import AboutSection from '@/features/dashboard/components/home/home-about/about-section';
 import GallerySection from '@/features/dashboard/components/home/home-gallery/gallery-section';
 import HomeContent from '@/features/dashboard/components/home/home-content';
-import { BestSellingSection } from '@/features/dashboard/components/home/home-products/bestselling-section';
+
 import { MostPopularSection } from '@/features/dashboard/components/home/home-products/mostpopular-section';
+import { BestSellingSection } from '@/features/dashboard/components/home/home-products/bestselling-section';
 
 export default async function HomePage() {
   return (
