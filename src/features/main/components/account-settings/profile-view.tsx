a611833ea397fb19e3142ec23584cf5e1e3a58eb
RowidaElshenawy <rowidaalielshenawy@gmail.com>
2026-08-12 23:45:40 +0300

    account footer

--- src/features/main/components/account-settings/profile-view.tsx
+++ src/features/main/components/account-settings/profile-view.tsx
@@ -1,6 +1,15 @@
 'use client';
-import { Camera } from 'lucide-react';
+
+import { Button } from '@/shared/components/ui/button';
 
 export default function ProfileView() {
-  return <div>profile</div>;
+  return (
+    <form>
+      <div></div>
+      <footer className="flex justify-between">
+        <Button buttonVariant="text" variant="ghost" title="Delete My Account" />
+        <Button type="submit" buttonVariant="text" variant="primary" title="Save Changes" />
+      </footer>
+    </form>
+  );
 }
