0e48f09ec0a01a56ab4f1f35838d38d59af03112
Menna-sultan <mennasultan491@gmail.com>
2026-08-13 14:54:02 +0300

    orders2

--- src/features/main/components/orders/order-items-container.tsx
+++ src/features/main/components/orders/order-items-container.tsx
@@ -6,6 +6,7 @@ import { useTranslations } from 'next-intl';
 
 import { OrderItemRow } from './order-item-row';
 import { OrderItem } from '../../types/order';
+import { Button } from '@/shared/components/ui/button';
 
 interface OrderItemsContainerProps {
   items: OrderItem[];
@@ -42,15 +43,15 @@ export function OrderItemsContainer({ items }: OrderItemsContainerProps) {
       </div>
 
       {hasHiddenItems && (
-        <button
+        <Button
           type="button"
+          variant="ghost"
+          buttonVariant="text"
           onClick={() => setIsExpanded((previous) => !previous)}
-          className="flex items-center justify-center gap-2 py-4 font-medium text-text-primary "
-        >
-          {isExpanded ? t('showLess') : t('showAll')}
-
-          {isExpanded ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
-        </button>
+          title={isExpanded ? t('showLess') : t('showAll')}
+          rightIcon={isExpanded ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
+          className="w-full py-4 font-medium text-text-primary"
+        />
       )}
     </div>
   );
