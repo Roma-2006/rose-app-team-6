abd56cf49075fec55ccb1ebc9bb4e2c19d6a607a
Asmaa Abdelfatah <abdoasmaa959@gmail.com>
2026-08-17 12:53:12 +0300

    .

--- src/features/user/schemas/change-pass.schema.ts
+++ src/features/user/schemas/change-pass.schema.ts
@@ -1,22 +1,27 @@
 'use server';
-import { getTranslations } from 'next-intl/server';
+import type { TranslationValues } from 'next-intl';
 import z from 'zod';
-
-const t = getTranslations('auth.auth-register.create-password.errors');
+type Translate = (key: string, values?: TranslationValues) => string;
 
 export const CHANGE_PASS_SCHEMA = z
   .object({
-    oldPassword: z.string().nonempty(t('confirm-password')),
+    oldPassword: z
+      .string()
+      .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
 
     newPassword: z
       .string()
       .min(8, '.password-min-length')
-      .regex(/[A-Z]/, t('password-uppercase'))
-      .regex(/[a-z]/, t('password-lowercase'))
-      .regex(/[0-9]/, t('password-number'))
-      .regex(/[^A-Za-z0-9]/, t('password-special')),
+      .min(1, { message: t('schema.username.required') })
 
-    confirmNewPassword: z.string().nonempty(t('confirm-password')),
+      .regex(/[A-Z]/, t('auth.auth-register.create-password.errors.password-uppercase'))
+      .regex(/[a-z]/, t('auth.auth-register.create-password.errors.password-lowercase'))
+      .regex(/[0-9]/, t('auth.auth-register.create-password.errors.password-number'))
+      .regex(/[^A-Za-z0-9]/, t('auth.auth-register.create-password.errors.password-special')),
+
+    confirmNewPassword: z
+      .string()
+      .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
   })
   .refine((data) => data.newPassword === data.confirmNewPassword, {
     message: 'Passwords do not match',
