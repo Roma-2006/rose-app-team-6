38d3c48f32f0ffc7d4b1818865bc615720d3b922
Asmaa Abdelfatah <abdoasmaa959@gmail.com>
2026-08-17 12:59:02 +0300

    .

--- src/features/user/schemas/change-pass.schema.ts
+++ src/features/user/schemas/change-pass.schema.ts
@@ -3,27 +3,27 @@ import type { TranslationValues } from 'next-intl';
 import z from 'zod';
 type Translate = (key: string, values?: TranslationValues) => string;
 
-export const CHANGE_PASS_SCHEMA = z
-  .object({
-    oldPassword: z
-      .string()
-      .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
+export const CHANGE_PASS_SCHEMA = (t: Translate) =>
+  z
+    .object({
+      oldPassword: z
+        .string()
+        .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
 
-    newPassword: z
-      .string()
-      .min(8, '.password-min-length')
-      .min(1, { message: t('schema.username.required') })
+      newPassword: z
+        .string()
+        .min(8, { message: t('auth.auth-register.create-password.errors.password-min-length') })
 
-      .regex(/[A-Z]/, t('auth.auth-register.create-password.errors.password-uppercase'))
-      .regex(/[a-z]/, t('auth.auth-register.create-password.errors.password-lowercase'))
-      .regex(/[0-9]/, t('auth.auth-register.create-password.errors.password-number'))
-      .regex(/[^A-Za-z0-9]/, t('auth.auth-register.create-password.errors.password-special')),
+        .regex(/[A-Z]/, t('auth.auth-register.create-password.errors.password-uppercase'))
+        .regex(/[a-z]/, t('auth.auth-register.create-password.errors.password-lowercase'))
+        .regex(/[0-9]/, t('auth.auth-register.create-password.errors.password-number'))
+        .regex(/[^A-Za-z0-9]/, t('auth.auth-register.create-password.errors.password-special')),
 
-    confirmNewPassword: z
-      .string()
-      .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
-  })
-  .refine((data) => data.newPassword === data.confirmNewPassword, {
-    message: 'Passwords do not match',
-    path: ['confirmNewPassword'],
-  });
+      confirmNewPassword: z
+        .string()
+        .nonempty(t('auth.auth-register.create-password.errors.confirm-password')),
+    })
+    .refine((data) => data.newPassword === data.confirmNewPassword, {
+      message: 'Passwords do not match',
+      path: ['confirmNewPassword'],
+    });
